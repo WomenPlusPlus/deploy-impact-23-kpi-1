6233feda4b1e4d2fbc31d5ba90fe3f10f853abca
giangpham-cfg <giang.pham1610@gmail.com>
2023-09-30 12:14:46 +0200

    create Sidebar component

--- KPI-team1-frontend/src/App.tsx
+++ KPI-team1-frontend/src/App.tsx
@@ -3,13 +3,13 @@ import Sidebar from "./components/Sidebar";
 
 export default function App() {
   return (
-    <>
-      <div className=".ml-5">
+    <div className="grid grid-cols-5 ">
+      <div className="col-span-1">
         <Sidebar />
       </div>
-      <div>
+      <div className="col-span-4">
         <Outlet />
       </div>
-    </>
+    </div>
   );
 }
