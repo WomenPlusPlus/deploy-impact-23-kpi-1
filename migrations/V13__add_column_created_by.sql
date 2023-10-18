ff68cf9b10596acf2249e798c9e34ef42bb66ef7
Daniel Czosnek <daczosnek@gmail.com>
2023-10-18 20:54:46 +0200

    Fix migration errors

--- migrations/V13__add_column_created_by.sql
+++ migrations/V13__add_column_created_by.sql
@@ -1,9 +1,15 @@
 ALTER TABLE
     kpi_definition
-ADD
-    COLUMN IF NOT EXISTS created_by uuid;
+DROP
+    CONSTRAINT IF EXISTS fk_auth_user;
 
 ALTER TABLE
     kpi_definition
 ADD
-    CONSTRAINT IF NOT EXISTS fk_auth_user FOREIGN KEY (created_by) REFERENCES auth.users (id);
+    COLUMN IF NOT EXISTS created_by uuid;
+
+
+ALTER TABLE
+    kpi_definition
+ADD
+    CONSTRAINT fk_auth_user FOREIGN KEY (created_by) REFERENCES auth.users (id);
