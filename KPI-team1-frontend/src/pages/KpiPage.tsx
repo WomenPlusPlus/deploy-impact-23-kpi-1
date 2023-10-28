4cb29f429454680ce9b71f35f7849ce2f2b0daa2
Aurelie <42074299+Aurelieph@users.noreply.github.com>
2023-10-28 12:38:47 +0200

    add percentage change and which kpi need update (#50)

--- KPI-team1-frontend/src/pages/KpiPage.tsx
+++ KPI-team1-frontend/src/pages/KpiPage.tsx
@@ -2,14 +2,18 @@ import { useEffect, useState } from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { KpiExtended } from "../model/kpi";
 import KpiDetailModalPage from "./KpiModalPage/KpiDetailModalPage";
-import { getDisplayValueByPeriodicity } from "../helpers/kpiHelpers";
+import {
+  getDisplayValueByPeriodicity,
+  getPreviousPeriodByPeriodicity,
+  getStringDisplayValueByPeriodicity,
+} from "../helpers/kpiHelpers";
 import { useOutletContext, useParams } from "react-router-dom";
 import { Circles } from "../model/circle";
 import { UserDetails } from "../model/user";
 import { HiOutlinePlusCircle } from "react-icons/hi";
 import AddKpiModalPage from "./AddKpiModalPage";
 import { HiMiniInformationCircle } from "react-icons/hi2";
-import { Tooltip } from "@mui/material";
+import { Grid, Tooltip } from "@mui/material";
 
 interface OutletContext {
   circles: Circles[];
@@ -51,19 +55,55 @@ const HEADER_KPI_COLUMNS: GridColDef[] = [
   {
     headerName: "Latest Value",
     field: "latest_value",
-    width: 150,
+    width: 200,
     sortable: true,
     headerAlign: "center",
     align: "center",
     renderCell: (params) => {
       const value = params.value as number;
-      if (value === null) {
-        return null;
-      } else if (params.row.unit === "%") {
-        return value.toFixed(2) + "%";
-      } else {
-        return value.toLocaleString("fr-ch");
-      }
+      const getDisplayValue = (value: number) => {
+        if (value === null) {
+          return null;
+        } else if (params.row.unit === "%") {
+          return value.toFixed(2) + "%";
+        } else {
+          return value.toLocaleString("fr-ch");
+        }
+      };
+      const getChangeColor = (value: number) => {
+        if (value === null) {
+          return "";
+        } else if (value > 0) {
+          return "bg-green-600";
+        } else if (value < -50) {
+          return "bg-red-600";
+        } else {
+          return "bg-amber-500";
+        }
+      };
+      return (
+        <Grid container className="flex justify-center items-center">
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4} className="mr-1 text-center">
+            {getDisplayValue(value)}{" "}
+          </Grid>
+          {params.row.percentage_change ? (
+            <Grid
+              item
+              xs={4}
+              className={`w-[60px] h-[29px] p-1.5 ${getChangeColor(
+                params.row.percentage_change
+              )} text-red-600 bg-opacity-50 rounded justify-center items-center gap-1 inline-flex`}
+            >
+              <div className="text-center text-neutral-900 text-[12px] font-medium font-['Inter']">
+                {params.row.percentage_change?.toFixed(2)}%
+              </div>
+            </Grid>
+          ) : (
+            <Grid item xs={4}></Grid>
+          )}
+        </Grid>
+      );
     },
   },
   {
@@ -80,7 +120,27 @@ const HEADER_KPI_COLUMNS: GridColDef[] = [
       if (date === null || periodicity === undefined) {
         return null;
       } else {
-        return getDisplayValueByPeriodicity(periodicity, date);
+        return (
+          <div className="flex" data-tooltip-target="tooltip-default">
+            {getStringDisplayValueByPeriodicity(periodicity, date) !==
+              getStringDisplayValueByPeriodicity(periodicity, new Date()) &&
+              getStringDisplayValueByPeriodicity(periodicity, date) !==
+                getPreviousPeriodByPeriodicity(periodicity, new Date()) && (
+                <Tooltip title={<span>Missing Latest Value</span>}>
+                  <svg
+                    className="w-[20px] h-[20px] text-gray-800 mr-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#F23030"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+                  </svg>
+                </Tooltip>
+              )}
+            {getDisplayValueByPeriodicity(periodicity, date)}{" "}
+          </div>
+        );
       }
     },
   },
@@ -100,6 +160,19 @@ const HEADER_KPI_COLUMNS: GridColDef[] = [
     sortable: false,
     headerAlign: "center",
     align: "center",
+    renderCell: (params) => {
+      const value = params.value as number;
+      const getDisplayValue = (value: number) => {
+        if (value === null) {
+          return null;
+        } else if (params.row.unit === "%") {
+          return value.toFixed(2) + "%";
+        } else {
+          return value.toLocaleString("fr-ch");
+        }
+      };
+      return <div>{getDisplayValue(value)}</div>;
+    },
   },
   {
     headerName: "",
@@ -194,9 +267,14 @@ export default function KpiPage(): JSX.Element {
 
     return (
       <div key={periodicity} className="mt-5">
-        <div className="text-xl font-medium">{`${periodicity
-          .charAt(0)
-          .toUpperCase()}${periodicity.slice(1)} KPIs`}</div>
+        <div className="flex items-end">
+          <div className="text-xl font-medium mr-2">{`${periodicity
+            .charAt(0)
+            .toUpperCase()}${periodicity.slice(1)} KPIs`}</div>{" "}
+          <span className="text-gray-400 text-xs font-semibold italic">
+            {getStringDisplayValueByPeriodicity(periodicity, new Date())}
+          </span>
+        </div>
         <div className="shadow-md border-0 border-primary-light mb-10">
           <DataGrid
             getRowId={(row) => row.circle_kpidef_id}
